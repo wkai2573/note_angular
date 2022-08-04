219cbadac894d8f694d672f640eabb185ae17a99
wkai <wkai2573@gmail.com>
2022-08-04 16:34:03 +0800

    1

--- app元件範本/服務請求2_user.service.ts
+++ app元件範本/服務請求2_user.service.ts
@@ -1,7 +1,3 @@
-import {HttpClient} from '@angular/common/http';
-import {Injectable} from '@angular/core';
-import {catchError, Observable, of} from 'rxjs';
-
 //使用者
 export interface User {
 	id: number;
